ed090fc8caa7609df154518051d553f92004e332
wei <1358079615@qq.com>
2016-04-27 16:48:16 +0800

    edit

--- todoapp_test/src/main/java/com/yappam/todoapp_test/BasePresenter.java
+++ todoapp_test/src/main/java/com/yappam/todoapp_test/BasePresenter.java
@@ -4,6 +4,5 @@ package com.yappam.todoapp_test;
  * Created by wei on 4/27/2016.
  *
  */
-public interface BaseView<T> {
-    void setPresenter(T presenter);
+public class BasePresenter {
 }
--- todoapp_test/src/main/java/com/yappam/todoapp_test/MainActivity.java
+++ todoapp_test/src/main/java/com/yappam/todoapp_test/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yappam.todoapp_test;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SystemClock.currentThreadTimeMillis();
 
 
     }
